e26a96fdb1efebd5877c2480f4061f46c1ea3f68
オノヤスヒロ <47861810+cant-dev@users.noreply.github.com>
2021-03-18 23:12:06 +0900

    Statesを実装 可変の状態を表す

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import ColorFulMessage from "./components/ColorfulMessage";
 
 const App = () => {
-  const onClickButton = () => alert();
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
+  const [num, setNum] = useState(0);
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは！</h1>
       <ColorFulMessage color="blue">お元気ですか？</ColorFulMessage>
       <ColorFulMessage color="pink">元気です</ColorFulMessage>
-      <button onClick={onClickButton}>ボタン</button>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <p>{num}</p>
     </>
   );
 };
